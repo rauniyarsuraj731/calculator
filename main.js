3a7c97cda0cf5167b9cc0f7422a25af59920cf8e
rauniyarsuraj731 <97456189+rauniyarsuraj731@users.noreply.github.com>
2022-03-06 18:49:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const operation1 = require("./add");
+
+const operation2 = require("./subtract");
+
+const operation3 = require("./multiply");
+
+const operation4 = require("./divide");
+
+
+
+let a = operation1(5,3);
+let b = operation2(8,4);
+let c = operation3(5,8);
+let d = operation4(4,20);
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
